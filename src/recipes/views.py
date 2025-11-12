794441f5def971fe5b494dd00b6655bab33a3460
CincinnatiKid428 <pweaver.psu03@gmail.com>
2025-11-11 19:23:00 -0500

    Exercise 2.4 commit
    Added view and template to recipe-app for home page.

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#View function for the recipe application homepage
+def homepage(request):
+    return render(request, 'recipes/recipes_home.html')
